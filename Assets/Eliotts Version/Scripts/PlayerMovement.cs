625751d23c56ef2f92d4cf77055b3804e7b1e8b6
HeroicDragon <epicdudeness677@gmail.com>
2019-10-16 19:33:46 +0200

    Included Eliotts version of Player Movement into the project

---
+++ Assets/Eliotts Version/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalMove =  Input.GetAxisRaw("Horizontal")* runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump") & jump != true)
+        {
+
+            animator.SetBool("jump", true);
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            animator.SetBool("crouching", true);
+            crouch = true;
+
+            
+        }else if (Input.GetButtonUp("Crouch"))
+            {
+            animator.SetBool("crouching", false);
+                crouch = false;
+            }
+       
+    }
+    public void OnLanding()
+        {
+            animator.SetBool("jump", false);
+        }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+    }
+}
---
+++ Assets/Eliotts Version/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public int damage;
+    public LayerMask whatisSolid;
+
+    public GameObject destroyEffect;
+
+    public void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+    private void Update()
+    {
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.up, distance, whatisSolid);
+        if (hitinfo.collider != null)
+        {
+            if (hitinfo.collider.CompareTag("Enemy"))
+            {
+                hitinfo.collider.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            DestroyProjectile();
+        }
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}

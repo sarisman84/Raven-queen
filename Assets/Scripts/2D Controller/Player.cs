76b5c38d9df404c37088a4a5ed2a8e8da97d85d5
Spyro <epicdudeness677@gmail.com>
2019-12-20 17:09:25 +0100

    Implemented Unity Event Callbacks.
    Created Unity Event Callbacks and implemented them into the PlayerController2D script.

---
+++ Assets/Scripts/2D Controller/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void DebugTest(){
+        Debug.Log("An event called me.");
+    }
+}

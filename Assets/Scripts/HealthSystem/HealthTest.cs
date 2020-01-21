a65f5d670d3f5f1ca0ad3dceae488817a5b946c2
Jos <joseph.dominguezmarrero@gmail.com>
2020-01-21 13:52:33 +0100

    HealthStats

---
+++ Assets/Scripts/HealthSystem/HealthTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTest : MonoBehaviour
+{
+    [SerializeField] CharacterHealthStats healthStats;
+    [SerializeField] Resistance.DamageType testType;
+    [SerializeField] float testDamage;
+    public void TestDamage()
+    {
+        healthStats.DealDamage(testDamage, testType);
+    }
+
+}
---
+++ Assets/Scripts/HealthSystem/HealthTestEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HealthTest))]
+[CanEditMultipleObjects]
+public class HealthTestEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        HealthTest healthTest = (HealthTest)target;
+        if (GUILayout.Button("Damage"))
+        {
+            healthTest.TestDamage();
+        }
+    }
+}

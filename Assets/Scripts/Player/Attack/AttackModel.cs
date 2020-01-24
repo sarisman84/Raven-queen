dc37046d0b5165cd917cd62133e4690f005cd088
Spyro_Mobile <epicdudeness677@gmail.com>
2020-01-24 15:10:01 +0100

    Spyro - Implemented {Basis of an Attack Template }
    Implemented Changes;
    
    -Delegate and struct that one can use to call upon different attack methods.
    -A test class to test the methods and statistics.

---
+++ Assets/Scripts/Player/Attack/AttackModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public delegate AttackInfo Attack ();
+public struct AttackInfo {
+    public GameObject owner;
+    public float damage;
+}
+
+public class AttackModel : MonoBehaviour {
+
+    public InputAction attackButton;
+    Attack attackBehaivour;
+
+    private void Awake () {
+        attackBehaivour = AttackMethod;
+        if (attackButton == null) throw new NullReferenceException ($"No button is set in the inspector -> {gameObject.name}");
+        attackButton.Enable ();
+    }
+
+    private AttackInfo AttackMethod () {
+        AttackInfo info = new AttackInfo ();
+        info.damage = 10f;
+        info.owner = gameObject;
+
+        Debug.Log ($"{info.owner.name} has dealt {info.damage}. ");
+
+        return info;
+    }
+
+    private void Update () {
+        if (attackButton.triggered) attackBehaivour ();
+    }
+}
